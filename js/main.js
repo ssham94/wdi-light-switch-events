bee3578d6225fb384986a415362381cf92b2f69b
Stanley Sham <stanley.sham94@gmail.com>
2019-08-20 14:16:36 -0400

    completed assignment

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const status = document.querySelector('.status');
+    const lightSwitch = document.querySelector('.switch')
+    const body = document.querySelector('body')
 
+    lightSwitch.addEventListener('click', function(e){
+        if (lightSwitch.getAttribute('class') === 'switch on'){
+            lightSwitch.setAttribute('class', 'switch off')
+            body.setAttribute('class', 'dark')
+            status.innerHTML = 'Hey, who turned off the lights?'
+        } else if (lightSwitch.getAttribute('class') === 'switch off'){
+            lightSwitch.setAttribute('class', 'switch on')
+            body.setAttribute('class', 'light')
+            status.innerHTML = 'It\'s so bright in here!'
 
+        }
+    })
+})
